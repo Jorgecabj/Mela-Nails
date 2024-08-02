3086e62521f447c6d7a4d79c33587f99f2684044
Jorgecabj <145295725+Jorgecabj@users.noreply.github.com>
2024-08-02 20:12:58 -0300

    agrego de js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Seleccionar el botón por su ID
+const boton = document.getElementById('miBoton');
+
+// Añadir un event listener para el evento 'click'
+boton.addEventListener('click', function() {
+    alert('¡De momento no hay mas turno disponibles, intenta mas tarde!');
+});
